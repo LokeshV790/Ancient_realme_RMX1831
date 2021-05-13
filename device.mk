42d09b4efa50816fed6520f988ae019ad6829b64
Radeon790 <lokeshvazirani29@gmail.com>
2021-05-13 05:07:22 +0000

    RMX1831: Official Ancient A11 Bringup!

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ancient
 
 # Permissions
 PRODUCT_COPY_FILES += \
